1d214a89232d007361d7f151b1dcdcdfa86ff270
ahmed-salama <as.theideal@gmail.com>
2024-07-31 17:19:13 +0300

    Updates

---
+++ static/js/571.c06eeb60.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[571],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=571.c06eeb60.chunk.js.map
