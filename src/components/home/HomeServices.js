22fb9d5904f066f720ee5fa7d606e4e48300668f
ahmed-salama <as.theideal@gmail.com>
2024-08-21 18:12:55 +0300

    update

--- src/components/home/HomeServices.js
+++ src/components/home/HomeServices.js
@@ -1,75 +1,62 @@
-import React from "react";
 import home from "./home.module.css";
 import timesaving from "../../assets/time.png";
 import highquality from "../../assets/highQuality.png";
 import homeworks from "../../assets/exams.png";
 import qanda from "../../assets/faq.png";
-import services_banner from "../../assets/view-3d-male-chemist-lab.png";
-import { motion } from "framer-motion";
 
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import "swiper/css/autoplay";
 function HomeBenefits() {
-  const animate = {
-    hidden: {
-      y: "-100px",
-      opacity: 0,
-      scale: 0.9,
-    },
-    visible: {
-      y: "0",
-      opacity: 1,
-      scale: 1,
-      transition: {
-        duration: 0.5,
-      },
-    },
-  };
   return (
     <div className={home.services}>
-      <img src={services_banner} alt="banner" />
-      <div className={home.cards}>
-        <motion.div
-          variants={animate}
-          initial="hidden"
-          animate="visible"
-          className={home.card}
-        >
-          <img src={timesaving} alt="service_img" />
-          <p>وفر وقتك الغالي</p>
-        </motion.div>
+      {/* <img src={services_banner} alt="banner" /> */}
+      <Swiper
+        style={{ width: "100%", height: "fit-content" }}
+        // install Swiper modules
+        modules={[Navigation, Pagination, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <div className={home.card}>
+            <img src={timesaving} alt="service_img" />
+            <p>وفر وقتك الغالي</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={home.card}>
+            <img src={highquality} alt="service_img" />
+            <p>محتوى على اعلى مستوى</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={home.card}>
+            <img src={homeworks} alt="service_img" />
 
-        <motion.div
-          variants={animate}
-          initial="hidden"
-          animate="visible"
-          className={home.card}
-        >
-          <img src={highquality} alt="service_img" />
-          <p>محتوى على اعلى مستوى</p>
-        </motion.div>
+            <p>واجبات و امتحانات دورية</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={home.card} style={{ zIndex: 2 }}>
+            <img src={qanda} alt="service_img" />
 
-        <motion.div
-          variants={animate}
-          initial="hidden"
-          animate="visible"
-          className={home.card}
-        >
-          <img src={homeworks} alt="service_img" />
-
-          <p>واجبات و امتحانات دورية</p>
-        </motion.div>
-        <motion.div
-          variants={animate}
-          initial="hidden"
-          animate="visible"
-          className={home.card}
-          style={{ zIndex: 2 }}
-        >
-          <img src={qanda} alt="service_img" />
-
-          <p>اسأل و احنا نجاوبك</p>
-        </motion.div>
-      </div>
-      <div className={home.services_banner}></div>
+            <p>اسأل و احنا نجاوبك</p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+      {/* <div className={home.services_banner}></div> */}
     </div>
   );
 }
